8b1433e2c9dc6bc488b349aaf3d4292aabd0adff
joethyr <joethayr@gmail.com>
2022-06-05 23:05:22 -0600

    update index action in controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,10 +3,10 @@ class StaticPagesController < ApplicationController
 
   def index
     @flickr = Flickr.new
-    if !params[:user_id].blank?
-      @photos = flickr.photos.search(user_id: params[:user_id])
+    if params[:user_id].present?
+      @photos = @flickr.photos.search(user_id: params[:user_id])
     else
-      @photos = flickr.photos.getRecent
+      @photos = @flickr.photos.getRecent
     end
   end
 end
