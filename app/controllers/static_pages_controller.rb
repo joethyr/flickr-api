d78e341bc744a94b1afb9e919a3b30d37224a4bf
joethyr <joethayr@gmail.com>
2022-06-06 09:12:45 -0600

    create partial file and move in index form

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   require 'flickr'
 
   def index
-    @flickr = Flickr.new
+    @flickr = Flickr.new(Figaro.env.flickr_key, Figaro.env.flickr_secret)
     if params[:user_id].present?
       @photos = @flickr.photos.search(user_id: params[:user_id])
     else
