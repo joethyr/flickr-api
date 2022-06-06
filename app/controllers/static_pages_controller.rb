b1ca5ac8dcf9764d32ec9e94111ae2f6d90249d9
joethyr <joethayr@gmail.com>
2022-06-06 10:40:50 -0600

    add rescue excemption to index action

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,11 +2,14 @@ class StaticPagesController < ApplicationController
   require 'flickr'
 
   def index
-    @flickr = Flickr.new(Figaro.env.flickr_key, Figaro.env.flickr_secret)
-    if params[:user_id].present?
-      @photos = @flickr.photos.search(user_id: params[:user_id])
-    else
+    begin
+      @flickr = Flickr.new(Figaro.env.flickr_key, Figaro.env.flickr_secret)
+      return @photos = @flickr.photos.search(user_id: params[:user_id]) if params[:user_id].present?
+
       @photos = @flickr.photos.getRecent
+    rescue StandardError => e
+      flash[:alert] = "#{e.class}: #{e.message}."
+      redirect_to root_path
     end
   end
 end
